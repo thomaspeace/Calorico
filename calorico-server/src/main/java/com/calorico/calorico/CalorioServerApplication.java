8be091c13ce03ae36744fa3db826360e523f5aa3
Tom Peace <thomasjpeace29@gmail.com>
2024-08-09 20:44:22 +0100

    add initial server

---
+++ calorico-server/src/main/java/com/calorico/calorico/CalorioServerApplication.java
@@ -0,0 +1,13 @@
+package com.calorico.calorico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorioServerApplication.class, args);
+	}
+
+}
