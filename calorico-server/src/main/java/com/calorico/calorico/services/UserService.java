b4902d3be83b4a828bc578b83bca7f080fc04e0f
Tom Peace <thomasjpeace29@gmail.com>
2024-08-10 19:07:43 +0100

    add initial services in User

--- calorico-server/src/main/java/com/calorico/calorico/services/UserService.java
+++ calorico-server/src/main/java/com/calorico/calorico/services/UserService.java
@@ -1,4 +1,26 @@
 package com.calorico.calorico.services;
 
+import com.calorico.calorico.models.User;
+import com.calorico.calorico.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+
 }
