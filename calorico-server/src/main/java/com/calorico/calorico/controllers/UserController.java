3d84e0a8d05fc43f56532577be35621fbef670ad
Tom Peace <thomasjpeace29@gmail.com>
2024-08-10 19:41:34 +0100

    add inital controller for User

--- calorico-server/src/main/java/com/calorico/calorico/controllers/UserController.java
+++ calorico-server/src/main/java/com/calorico/calorico/controllers/UserController.java
@@ -1,4 +1,39 @@
 package com.calorico.calorico.controllers;
 
+import com.calorico.calorico.models.User;
+import com.calorico.calorico.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> user = userService.getUserById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
