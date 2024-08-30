55ac0b06f1887a6f3591e9b3d4dec5e61aec259e
Tom Peace <thomasjpeace29@gmail.com>
2024-08-30 19:11:16 +0100

    add proper modal functionality

--- calorico-client/src/components/WeightAddModal.jsx
+++ calorico-client/src/components/WeightAddModal.jsx
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { addWeightToUser } from "../api";
 
@@ -8,11 +8,15 @@ const WeightAddModal = ( { userId } ) => {
     const [dateWeighed, setDateWeighed] = useState('');
     const [weightMetric, setWeightMetric] = useState('');
 
+    const queryClient = useQueryClient();
+
     const mutation = useMutation({
         mutationFn: addWeightToUser,
         onSuccess: () => {
             setDateWeighed('');
             setWeightMetric('');
+            queryClient.invalidateQueries(['weights']);
+            document.getElementById('weight_add_modal').close()
         },
         onError: (error) => {
             alert(`Error: ${error.message}`);
@@ -37,7 +41,7 @@ const WeightAddModal = ( { userId } ) => {
                 {/* if there is a button in form, it will close the modal */}
                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 </form>
-                <h3 className="font-bold text-lg">Hello!</h3>
+                <h3 className="font-bold text-lg">New Weight:</h3>
                 {/* <p className="py-4">Press ESC key or click on ✕ button to close</p> */}
                 <form onSubmit={handleSubmit}>
                     <input 
@@ -54,7 +58,7 @@ const WeightAddModal = ( { userId } ) => {
                         step={0.1} 
                         required 
                     />
-                    <button type="submit" disabled={mutation.isLoading}>
+                    <button className='btn' type="submit" disabled={mutation.isLoading}>
                         Add Weight
                     </button>
                 </form>
